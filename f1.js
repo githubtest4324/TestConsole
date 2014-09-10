8ae4776dc89620b736ec42c7621b88b64c865f7d
gliviu <grigoresculiviu@gmail.com>
2014-09-10 06:45:31 +0300

    console

--- f1.js
+++ f1.js
@@ -1,47 +1,62 @@
 module.exports = {
-    "ecom": {
-        "customers": {
-            "Customer": {
-                "type": "entity",
-                "properties": {
-                    "code": {
-                    	"type": "string"
+    ns1 : {
+        rest1 : {
+            type : 'rest',
+            url : 'url1',
+            input : 'str',
+            output : {
+                type : 'list',
+                itemType : 'str'
+            }
+        },
+        rest2 : {
+            type : 'rest',
+            url : 'url2',
+            p3 : 'p3',
+            input : {
+                type : 'entity',
+                properties : {
+                    f1 : 'str'
+                },
+                p2 : 'p2',
+            },
+            output : {
+                type : 'entity',
+                p1 : 'p1',
+                properties : {
+                    f1 : {
+                        type : 'entity',
+                        properties : {
+                            f2 : 'str'
+                        }
                     }
                 }
             }
         },
-        "regional": {
-            "Country": {
-                "type": "entity",
-                "properties": {
-                    "name": "str"
-                }
-            },
-            "City": {
-                "type": "entity",
-                "properties": {
-                    "name": "str",
-                    "code": {
-                        "type": "str",
-                        "translate": "code2"
-                    },
-                    "zipCodes": {
-                        "type": "list",
-                        "itemType": "str"
-                    }
+        rest3 : {
+            type : 'rest',
+            url : 'url3',
+            input : 'string',
+            output : true
+        },
+        rest4 : {
+            type : 'rest',
+            url : 'url3',
+            input : 100
+        },
+        rest5 : {
+            type : 'rest',
+        },
+        rest6: {
+            type: 'rest',
+            url: 'url4',
+            input: {
+                properties : {
+                    f1 : 'str'
                 }
             }
         }
-    },
-    "Request": {
-        "type": "entity",
-        "properties": {
-            "name": "str",
-            "code": {
-                "type": "str",
-                "translate": "code2"
-            },
-            "customer": "Customer"
-        }
+        
+        
     }
 };
---
+++ test.js
@@ -0,0 +1,6 @@
+var jst = require('./JsType');
+
+var x = '2015-01-07';
+var d = new Date(x);
+
+console.log(d);
